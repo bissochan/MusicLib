95067285352c3c4b338bcaac23cfe891a2f80a37
bissochan <lucabisso1902@gmail.com>
2025-02-15 11:52:43 +0100

    first commit

---
+++ MusicLib.Main/Program.cs
@@ -0,0 +1,22 @@
+﻿using MusicLib.Scripts;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please specify an argument.");
+            return;
+        }
+        
+        string url = args[0];
+        Console.WriteLine("Starting...");
+        Console.WriteLine("URL playlist:" + url);
+        
+        ScriptManager sManager = new ScriptManager();
+        sManager.RunSpotdl(url);
+        
+        
+    }
+}
---
+++ MusicLib.Scripts/ScriptManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MusicLib.Scripts;
+
+public interface IScriptManager
+{
+    void RunSpotdl(string urlPlaylist);
+}
+
+public class ScriptManager : IScriptManager
+{
+    public void RunSpotdl(string urlPlaylist)
+    {
+        
+        ProcessStartInfo psi = new ProcessStartInfo()
+        {
+            FileName = "spotdl",
+            Arguments = urlPlaylist,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = false,
+            UseShellExecute = false
+        };
+        
+        Console.WriteLine("Running Spotdl");
+
+        using (Process process = Process.Start(psi)!)
+        {
+            process.OutputDataReceived += (s, e) => { Console.WriteLine(e.Data); };
+            process.ErrorDataReceived += (s, e) => { Console.WriteLine(e.Data); };
+
+            process.Start();
+            
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            
+            process.WaitForExit();
+        }
+        
+        
+        
+    }
+}
